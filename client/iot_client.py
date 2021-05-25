feafbafde00286c0df42c930caede3455ae297c6
martin <martin@sirull.com>
2021-05-25 12:54:40 -0400

    Update README

--- client/iot_client.py
+++ client/iot_client.py
@@ -454,7 +454,7 @@ class IoTThing(AWSIoTMQTTClient):
         r = requests.get(
             'https://{0}.credentials.iot.{1}.amazonaws.com/role-aliases/{2}/credentials'.format(
                 os.environ['CREDENTIALS_ENDPOINT'], os.environ['AWS_DEFAULT_REGION'], self.shadow['tenant']), headers=iot_headers, cert=(cert_file.name, pk_file.name),
-            verify="/tmp/AmazonRootCA1.pem")
+            verify="AmazonRootCA1.pem")
         print(r.status_code)
         sts_credentials = r.json()['credentials']
         self.boto_session = boto3.Session(

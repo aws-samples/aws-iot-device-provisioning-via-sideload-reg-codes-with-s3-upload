e9c82ed2d8d7c9758f4bf4ba4613f8d65ec3be87
Martin Sirull <martin@sirull.com>
2021-08-08 11:22:31 -0700

    Update Github workflow
    Rewrite action with custom commands

--- api/infrastructure/app.py
+++ api/infrastructure/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+import os
 from aws_cdk import core as cdk
 from stacks.chaliceapp import ChaliceApp
 
+
 app = cdk.App()
-ChaliceApp(app, 'device-registration-api')
+ChaliceApp(app, os.environ.get('STACK_NAME', 'device-registration-api')
 
 app.synth()
